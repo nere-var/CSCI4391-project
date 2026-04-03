abc49df0f052d9bc383246422a4ec9a66fb0882a
Abigail Rodriguez Vazquez <picklefarm.123456@gmail.com>
2026-04-02 22:49:21 -0500

    Add functions to open and close chat window

---
+++ src/static/js/MainPageJS.js
@@ -0,0 +1,9 @@
+function openChatWindow(){
+    document.getElementById("chatBox").style.display ="block";
+
+}
+
+function closeChatWindow(){
+    document.getElementById("chatBox").style.display ="none";
+
+}
