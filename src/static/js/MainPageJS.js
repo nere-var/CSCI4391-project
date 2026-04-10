2835838aeabbb8afcdb48c2f75134efc0f78a057
Luis M <77978673+OrangeXR@users.noreply.github.com>
2026-04-10 00:31:09 -0500

    chat window stays open on response

--- src/static/js/MainPageJS.js
+++ src/static/js/MainPageJS.js
@@ -1,9 +1,9 @@
-function openChatWindow(){
-    document.getElementById("chatBox").style.display ="block";
-
+function openChatWindow() {
+    document.getElementById("chatBox").style.display = "block";
+    localStorage.setItem("chatOpen", "true"); /* Stores status of chatwindow */
 }
 
-function closeChatWindow(){
-    document.getElementById("chatBox").style.display ="none";
-
+function closeChatWindow() {
+    document.getElementById("chatBox").style.display = "none";
+    localStorage.setItem("chatOpen", "false"); /* Stores status of chatwindow */
 }
