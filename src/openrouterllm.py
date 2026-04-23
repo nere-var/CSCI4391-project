58934fbb47f6b071dd64a4bab62d5be6e06f839e
Luis M <77978673+OrangeXR@users.noreply.github.com>
2026-04-23 16:30:45 -0500

    Update openrouterllm.py

--- src/openrouterllm.py
+++ src/openrouterllm.py
@@ -9,11 +9,16 @@ from validator import recipe_validator   #importing everything from validator
 # ================
 # Global variables
 # ================
+
 # For the welcome message: "Welcome! The model is XXX . Type 'quit' to exit."
 model_name = "Ling-2.6-flash (free)"
-# For the actual AI usage: "inclusionai/ling-2.6-flash:free",
-ai_model_used = "inclusionai/ling-2.6-flash:free"
 
+# For the actual AI usage: "inclusionai/ling-2.6-flash:free",
+# ai_model_used = "nvidia/nemotron-nano-9b-v2:free"       # NVIDIA: Nemotron Nano 9B V2 (free)   # Going away ??
+# ai_model_used = "google/gemma-4-26b-a4b-it:free"        # Google: Gemma 4 26B A4B (free)       # Going away ??
+ai_model_used = "inclusionai/ling-2.6-flash:free"         # Ling-2.6-flash (free)                # Going away April 29, 2026
+# ai_model_used = "arcee-ai/trinity-large-preview:free"   # Trinity Large Preview (free)         # Going away April 22, 2026
+# ================
 
 class Ai_Chat: 
     # IMPORTANT
@@ -177,7 +182,7 @@ class Ai_Chat:
         # =                      [Welcome message showing the AI model used]                                            =
         # ===============================================================================================================
         #print("Welcome! The model is Trinity Large Preview (free). Type 'quit' to exit.\n")
-        print(f"Welcome! {model_name}. Type 'quit' to exit.\n")
+        print(f"Welcome! The model is {model_name}. Type 'quit' to exit.\n")
         # ===============================================================================================================   
         # this is where the chatbot will be constrained
         messages = [
